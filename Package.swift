ed74e4fd8c6ce1402eebf23f2bc10ec918609867
Alan Paiva <jpaiva.alan@gmail.com>
2023-07-19 21:04:55 -0300

    Update Plot dependency to 0.13.0

--- Package.swift
+++ Package.swift
@@ -15,14 +15,15 @@ let package = Package(
             targets: ["SocialButtons"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0")
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(name: "Plot", url: "https://github.com/JohnSundell/Plot.git", from: "0.13.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SocialButtons",
-            dependencies: ["Publish"]),
+            dependencies: ["Publish", "Plot"]),
         .testTarget(
             name: "SocialButtonsTests",
             dependencies: ["SocialButtons"]),
